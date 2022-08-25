a5b1352dba368862a2641845a1ba664a89841eff
Febrian SF <febriansf@student.uns.ac.id>
2022-08-25 21:08:13 +0700

    First Commit \nCreate PBO1

---
+++ Java/PBO 01/MyProject1/src/myproject1/MyProject1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject1;
+
+/**
+ *
+ * @author BrxxN
+ */
+public class MyProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello World!");
+        System.out.println("Hello Java");
+        System.out.println("Java is nuf");
+    }
+    
+}
---
+++ Java/PBO 01/MyProject2/src/myproject2/MyProject2.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author BrxxN
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+            
+    public static void main(String[] args) {
+//        String[] hurufB = { "BBBBBB ",
+//                            "BB   BB",
+//                            "BBBBBB ",
+//                            "BB   BB",
+//                            "BBBBBB "};
+//        
+//        String[] hurufR = { "RRRRRR ",
+//                            "RR   RR",
+//                            "RRRRRR ",
+//                            "RR  RR ",
+//                            "RR   RR"};
+//        
+//        String[] hurufI = { "II",
+//                            "II",
+//                            "II",
+//                            "II",
+//                            "II"};
+//        
+//        
+//        for (int i=0; i < 5; i++) {
+//            System.out.println("%s  %s  %s".formatted(hurufB[i], hurufR[i], hurufI[i]));
+//        }
+
+            System.out.println("BBBBBB   RRRRRR   II");
+            System.out.println("BB   BB  RR   RR  II");
+            System.out.println("BBBBBB   RRRRRR   II");
+            System.out.println("BB   BB  RR  RR   II");
+            System.out.println("BBBBBB   RR   RR  II");
+    }
+    
+}
