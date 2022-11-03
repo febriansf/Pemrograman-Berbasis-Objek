4e8e7ac421247b94efccd872a6255901229cf2d5
Febrian SF <febriansf@student.uns.ac.id>
2022-11-03 09:24:12 +0700

    Add PBO UTS

---
+++ Java/PBO UTS/ProjectUTS/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        newClass c = new newClass();
+
+        c.method(10, 1);
+        c.method(6, 5);
+        c.method(1, 1);
+    }
+}
---
+++ Java/PBO UTS/ProjectUTS/src/newClass.java
@@ -0,0 +1,6 @@
+public class newClass {
+    
+    void method (int a, int b){
+        System.out.println(a + b);
+    }
+}
